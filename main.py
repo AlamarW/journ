9a80190bbcd3b585ff85fca3f6cf07c1abe84a16
AlamarW <Austinlamarwiggins@gmail.com>
2024-08-22 14:23:51 -0600

    dug myself out of that one

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 import cmd
 import sqlite3
 from datetime import datetime
-from os import system, name
-<<<<<<< HEAD
 from tui_editor import TuiEditor
-=======
-import uuid
->>>>>>> 0e41595c6cbaf3d272a79731630b2b782e647d6c
+from os import system, name
 
-conn = sqlite3.connect('journal.db')
+conn = sqlite3.connect("journal.db")
 cursor = conn.cursor()
 cursor.execute("""CREATE TABLE IF NOT EXISTS user_info (
                 user_id integer,
@@ -28,6 +24,7 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS journal_session (
 
 conn.commit()
 
+
 class User:
     def __init__(self, user_id, password, writing_goal):
         self.user_id = user_id
@@ -38,14 +35,17 @@ class User:
         print(self.user_id)
         print(self.password)
 
-class JournalSession:
 
-    def __init__(self, session_id, journ_text, words_per_minute, accomplished_writing_goal, date):
+class JournalSession:
+    def __init__(
+        self, session_id, journ_text, words_per_minute, accomplished_writing_goal, date
+    ):
         self.session_id = session_id
         self.journ_text = journ_text
         self.words_per_minute = words_per_minute
         self.accomplished_writing_goal = accomplished_writing_goal
         self.date = date
+
 
 class JournalingShell(cmd.Cmd):
     intro_string_1 = "Welcome to Journ, type help or ? to list commands\n"
@@ -53,19 +53,23 @@ class JournalingShell(cmd.Cmd):
     intro_string_3 = "Note: don't forget to log in to save journ\n"
 
     intro = intro_string_1 + intro_string_2 + intro_string_3
-    prompt = '(journ) '
+    prompt = "(journ) "
     writing_goal = 0
     name = None
 
-    def do_login(self,line):
+    def do_login(self, line):
         "Register and Login here!"
-        #registration function needs to go here
+
+        # registration function needs to go here
         def confirm_login():
             def login():
                 """Ask user for user name, check it against database"""
                 user_name = input("What is your user name? ")
-                user_data = cursor.execute("""SELECT user_id, password, writing_goal FROM
-                   user_info WHERE user_id = ?""",[user_name])
+                user_data = cursor.execute(
+                    """SELECT user_id, password, writing_goal FROM
+                   user_info WHERE user_id = ?""",
+                    [user_name],
+                )
                 user_data_grab = user_data.fetchone()
                 password_actual = user_data_grab[1]
                 password_attempt = input("What is your password? ")
@@ -80,9 +84,13 @@ class JournalingShell(cmd.Cmd):
             def register():
                 user_name = input("choose your username ")
                 password = input("Choose your password ")
-                writing_goal = input("Write your daily writing goal (Using digits only) ")
-                cursor.execute("INSERT INTO user_info VALUES (?, ? , ?)",
-                               [user_name, password, int(writing_goal)])
+                writing_goal = input(
+                    "Write your daily writing goal (Using digits only) "
+                )
+                cursor.execute(
+                    "INSERT INTO user_info VALUES (?, ? , ?)",
+                    [user_name, password, int(writing_goal)],
+                )
                 conn.commit()
                 print("Now log in to the system")
                 login()
@@ -100,12 +108,11 @@ class JournalingShell(cmd.Cmd):
                 confirm_login()
 
         confirm_login()
-        #user_name = input("User name: ")
-        #password = input("password: ")
+        # user_name = input("User name: ")
+        # password = input("password: ")
 
-        #user = User(user_name, password, 23)
-        #user.register()
-
+        # user = User(user_name, password, 23)
+        # user.register()
 
     def user_info(user_name):
         return True
@@ -132,62 +139,58 @@ class JournalingShell(cmd.Cmd):
         end_time = datetime.now()
 
         elapsed_time = end_time - start_time
-<<<<<<< HEAD
 
+        in_seconds = elapsed_time.total_seconds()
+        in_minutes = in_seconds / 60
         time_string = str(elapsed_time)
 
         parsed_time = time_string.split(":")
-        print(f"""You've journalled for {parsed_time[0]} hour(s),
-            {parsed_time[1]} minute(s), and {parsed_time[2][:2]} seconds""")
-=======
-        in_seconds = elapsed_time.total_seconds() 
-        in_minutes = in_seconds/60
-        time_string = str(elapsed_time)
-
-        parsed_time = time_string.split(":")
-        print(f"You've journalled for {parsed_time[0]} hour(s), {parsed_time[1]} minute(s), and {parsed_time[2][:2]} seconds")
-        journ_wpm = round(journal_length / in_minutes, 1) 
+        print(
+            f"You've journalled for {parsed_time[0]} hour(s), {parsed_time[1]} minute(s), and {parsed_time[2][:2]} seconds"
+        )
+        journ_wpm = round(journal_length / in_minutes, 1)
         JournalSession.session_id = start_time
         JournalSession.journ_text = contents
         JournalSession.words_per_minute = journ_wpm
         JournalSession.date = end_time
-        cursor.execute("INSERT INTO journal_session VALUES (?, ? , ?, ?, ?)", [JournalSession.session_id, JournalSession.journ_text, JournalSession.words_per_minute,JournalSession.date,True])
+        cursor.execute(
+            "INSERT INTO journal_session VALUES (?, ? , ?, ?, ?)",
+            [
+                JournalSession.session_id,
+                JournalSession.journ_text,
+                JournalSession.words_per_minute,
+                JournalSession.date,
+                True,
+            ],
+        )
         conn.commit()
- 
->>>>>>> 0e41595c6cbaf3d272a79731630b2b782e647d6c
 
     def streak_details(self, user_name, login):
         raise NotImplementedError
 
     def do_test(self, line):
-<<<<<<< HEAD
         "Check DB Status, should be two tables (DELETE BEFORE FINAL RELEASE"
         cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        conn.commit()
+        cursor.execute("SELECT * FROM journal_session")
         print(cursor.fetchall())
         conn.commit()
-=======
-       "Check DB Status, should be two tables (DELETE BEFORE FINAL RELEASE"
-       cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-       print(cursor.fetchall())
-       conn.commit()
-       cursor.execute("SELECT * FROM journal_session")
-       print(cursor.fetchall())
-       conn.commit()
->>>>>>> 0e41595c6cbaf3d272a79731630b2b782e647d6c
 
     def save_journal(self, args):
         raise NotImplementedError
 
     def clear():
-        if name =='nt':
-            _ = system('cls')
+        if name == "nt":
+            _ = system("cls")
 
         else:
-            _ = system('clear')
+            _ = system("clear")
 
-    def do_EOF(self,line):
+    def do_EOF(self, line):
         return True
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     JournalingShell.clear()
     JournalingShell().cmdloop()
     conn.close()
